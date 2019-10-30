aabcd75ca1b59058f722f884b4b1b6b58dfeb219
Edgaras Dvariskis <edgdva@ktu.lt>
2019-10-30 16:57:32 +0200

    sukurta funkcija

--- hello.py
+++ hello.py
@@ -1,1 +1,9 @@
-print("Hello, student!")
+import sys  # importiname nuskaitymo biblioteka
+
+
+def greet(name: str):  # funkcija is uzduoties
+    return f"Hello, {name}!"  # vis dar ji
+
+
+if __name__ == "__main__":  # perduodame i argumenta kintamaji
+    print(greet(sys.argv[1]))  # jeigu [0], tai hello.py, jeigu 1, tai tas kas po hello.py
