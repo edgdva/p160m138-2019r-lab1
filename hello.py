37fe8117861cc17180d816a7cc152e6e8b3233af
Edgaras Dvariskis <edgdva@ktu.lt>
2019-10-30 17:43:43 +0200

    tekstas update

--- hello.py
+++ hello.py
@@ -1,7 +1,4 @@
 import sys
-
-
-<<<<<<< HEAD
 def greet(greeted_name: str, shout_count: int = 1):
     return f"Hello, {greeted_name}{shout_count * '!'}"
 
@@ -9,11 +6,4 @@ def greet(greeted_name: str, shout_count: int = 1):
 if __name__ == "__main__":
     n = int(sys.argv[2]) if len(sys.argv) == 3 else 1
     print(greet(sys.argv[1], n))
-=======
-def greet(greeted_name: str):  # funkcija is uzduoties
-    return f"Hello, {greeted_name}!"  # vis dar ji
 
-
-if __name__ == "__main__":  # perduodame i argumenta kintamaji
-    print(greet(sys.argv[1]))  # jeigu [0], tai hello.py, jeigu 1, tai tas kas po hello.py
->>>>>>> master
